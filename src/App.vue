617c7004a5d97ff070dd2b786a3cb7b04ecf8c09
SoyQuillo <santiagoquintero190305@gmail.com>
2023-11-15 07:39:01 -0500

    factorial

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+
+import { ref } from 'vue'
+
+let variableRef = ref(0);
+
+let aux = ref(0);
+
+let total = 1;
+
+function factorial (variableRef){
+
+  for(let i=1; i < variableRef +1; i++){
+
+    total = total *i; 
+  }
+
+  return total;
+
+}
+
+function doClick() {
+
+
+aux.value = variableRef.value;
+
+}
+
+</script>
+
+<template>
+
+  <div>
+    <input v-model="variableRef" type="number">
+    <button @click="doClick()" >PRESIONAME</button>
+    <h1>{{ factorial(aux) }}</h1>
+  </div>
+  
+</template>
+
+<style scoped>
+
+</style>
